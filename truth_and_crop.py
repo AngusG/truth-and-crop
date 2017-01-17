2bb160d860aaa12b5e479f8762addf05963a8add
Angus Galloway <premiumdriedfoods@gmail.com>
2017-01-16 20:32:12 -0500

    Use os.path.join, minor cleanups.

--- truth_and_crop.py
+++ truth_and_crop.py
@@ -1,4 +1,5 @@
-import cv2  # tested with opencv version 3.1.0, as low as 2.4.0 should work
+import cv2  # Tested with opencv version 3.0.0.
+import os.path
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
@@ -6,18 +7,22 @@ from skimage.segmentation import slic
 from skimage.segmentation import mark_boundaries
 
 #  Constants
-APP_NAME='Truth and Crop'
+APP_NAME = 'Truth and Crop'
+IMAGES_OUT_DIR = 'images/'
+MASKS_OUT_DIR = 'masks/'
+IMAGE_EXT = '.jpg'
+MASK_EXT = '_mask.jpg'
 PX_INTENSITY = 0.4
 N_CHANNELS = 2
 
 #  Globals
-drawing = False  # True if mouse is pressed
-cropping = False  # If True, draw rectangle. Press 'm' to toggle to curve
+drawing = False  # Set to True if not cropping.
+cropping = False  # Press 'm' to toggle, if True, draw rectangle.
 ix, iy = -1, -1
 w = 0
 
 crop_list = []
-class_label = 0  # 0 for other, 1 for mussel, 2 for tunicate
+class_label = 0
 drawing_list = []
 
 
@@ -42,13 +47,13 @@ def handle_mouse_events(event, x, y, flags, param):
     global w, drawing, cropping
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        # if we are cropping, we are not truthing
+        # If we are cropping, we are not truthing.
         if cropping == True:
             drawing = False
             cv2.rectangle(img, (x - w, y - w), (x + w, y + w), (0, 255, 0), 3)
             crop_list.append((x, y))
 
-        # we are ground truthing
+        # We are ground truthing.
         else:
             drawing = True
             drawing_list.append((x, y, class_label))
@@ -82,13 +87,13 @@ if __name__ == '__main__':
 
     w = args.wnd
 
-    input_file = args.img_path + args.img_name + ".JPG"
+    input_file = os.path.join(args.img_path, args.img_name)
     img = cv2.imread(input_file)[::args.ds, ::args.ds, :].astype(np.uint8)
 
-    # Copy original so we don't capture box outlines in cropped images
+    # Copy original so we don't capture box outlines in cropped images.
     original = img.copy()
 
-    # Initialize segmentation mask as "other" class
+    # Initialize segmentation mask as "other" class.
     segmentation_mask = np.zeros(img[:, :, 0].shape)
 
     segments = slic(img, n_segments=args.nseg, sigma=args.sigma)
@@ -102,35 +107,48 @@ if __name__ == '__main__':
         cv2.imshow(APP_NAME, img)
         key = cv2.waitKey(1) & 0xFF
 
-        # 'm' - change mode from cropping to drawing
+        # 'm' - Change mode from cropping to drawing.
         if key == ord('m'):
             cropping = not cropping
 
-        # 'w' - write all cropped regions and their segmentation masks'
+        # 'w' - Write all cropped regions and their segmentation masks'.
         elif key == ord('w'):
 
-            for px, py, pclass in drawing_list:
+            for px, py, p_class in drawing_list:
 
-                # find superpixel that coord belongs to
-                superpx = segments[py, px]
+                # Find superpixel that coord belongs to.
+                super_px = segments[py, px]
 
-                # set all pixels in superpx to pclass
-                segmentation_mask[segments == superpx] = pclass
+                # Set all pixels in super_px to p_class.
+                segmentation_mask[segments == super_px] = p_class
 
             i = 0
             for x, y in crop_list:
-                out_file = args.img_name + '_nseg' + str(args.nseg) + '_sig' + str(args.sigma) \
+
+                # Detailed cropped image suffix.
+                details = args.img_name[:-4] + '_nseg' + str(args.nseg) + '_sig' + str(args.sigma) \
                     + '_ds' + str(args.ds) + '_' + str(i) + \
                     "_x" + str(x) + "_y" + str(y)
-                crop_image = original[y - w:y + w, x - w:x + w, :]
-                crop_mask = segmentation_mask[y - w:y + w, x - w:x + w]
-                cv2.imwrite(args.out_path + "images/" +
-                            out_file + ".jpg", crop_image)
-                cv2.imwrite(args.out_path + "segmentations/" +
-                            out_file + "_mask.jpg", crop_mask)
+                cropped_image = original[y - w:y + w, x - w:x + w, :]
+                cropped_mask = segmentation_mask[y - w:y + w, x - w:x + w]
+
+                image_path = os.path.join(args.out_path, IMAGES_OUT_DIR)
+                mask_path = os.path.join(args.out_path, MASKS_OUT_DIR)
+
+                if not os.path.exists(image_path):
+                    os.makedirs(image_path)
+
+                if not os.path.exists(mask_path):
+                    os.makedirs(mask_path)
+
+                cv2.imwrite(os.path.join(
+                    image_path, details + IMAGE_EXT), cropped_image)
+                cv2.imwrite(os.path.join(
+                    mask_path, details + IMAGE_EXT), cropped_mask)
+
                 i += 1
 
-            print("Saved cropped images and masks")
+            print("Saved cropped images and masks.")
 
         elif key == 's':
             plt.close('all')
@@ -138,24 +156,24 @@ if __name__ == '__main__':
             fig = plt.figure()
             plt.imshow(segmentation_mask)
             plt.show()
-            print("Showing segmentation mask")
+            print("Showing segmentation mask.")
 
-        # '0' - change to class 0
+        # '0' - Change to class 0.
         elif key == ord('0'):
             class_label = 0
-            print("Selected class 0")
+            print("Selected class 0.")
 
-        # '1' - change to class 1
+        # '1' - Change to class 1.
         elif key == ord('1'):
             class_label = 1
-            print("Selected class 1")
+            print("Selected class 1.")
 
-        # '2' - change to class 2
+        # '2' - Change to class 2.
         elif key == ord('2'):
             class_label = 2
-            print("Selected class 2")
+            print("Selected class 2.")
 
-        # 'q' - quit, potentially without writing
+        # 'q' - Quit, potentially without writing.
         elif key == ord('q'):
             break
 
